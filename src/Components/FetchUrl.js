ef22bd568ab3b71fc2722e23801ba2f3bcc2b0b0
Pere <pere.solaclaver@gmail.com>
2020-07-22 00:08:21 +0100

    Redirect logic when param is passed to the url, with api call and using the returned url to to a redirect

--- src/Components/FetchUrl.js
+++ src/Components/FetchUrl.js
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const FetchUrl = (props) => {
+	const [redirect, setRedirect] = useState(false);
+	const [error, setError] = useState(false);
+	const [newUrl, setNewUrl] = useState(null);
+
+	// grab the param after '/' in the url because this points to the real URL in the server
 	const { shortUrl } = props.match.params;
+
+	// fetch real URL from the server by passing the url param
 	useEffect(() => {
 		axios
 			.get(`http://localhost:5000/${shortUrl}`)
 			.then((res) => {
-				debugger;
+				// if server returns data, we set state
+				if (res.data.status === 'success') {
+					setRedirect(true);
+					setNewUrl(res.data.longUrl);
+				} else if (res.data.status === 'fail') {
+					setError(true);
+					setRedirect(false);
+				}
 			})
+			// if server returns error
 			.catch((error) => {
-				debugger;
+				setError(true);
+				setRedirect(false);
 			});
-	}, []);
-	return <div>hello</div>;
+	}, [shortUrl]);
+	// when we have data back from server and not an error
+	if (redirect && !error) {
+		// redirect to new url
+		window.location.href = newUrl;
+		return <div>Redirecting...</div>;
+		// if we get no data from server (i.e. there is no short url stored with the param indicated by the User)
+	} else if (!redirect && error) {
+		return <div>There was an error</div>;
+		// fallback option
+	} else {
+		return <div>Checking url...</div>;
+	}
 };
 
 export default FetchUrl;
