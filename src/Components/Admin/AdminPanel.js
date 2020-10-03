59e678014143c5b4f3327cff0b625c311363d137
Pere <pere.solaclaver@gmail.com>
2020-10-03 21:55:29 +0100

    Minimum style to admin panel

--- src/Components/Admin/AdminPanel.js
+++ src/Components/Admin/AdminPanel.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import BackButton from './Navigation/BackButton';
+
+const StyledDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
 
 const AdminPanel = ({ usersToBeApproved, match }) => {
 	return (
 		<section>
-			<div>
+			<StyledDiv>
 				<Link to='/admin/approval'>
 					{`Users to be approved `}
 					<span>
@@ -16,7 +24,7 @@ const AdminPanel = ({ usersToBeApproved, match }) => {
 					</span>
 				</Link>
 				<Link to='/admin/active'>Users active</Link>
-			</div>
+			</StyledDiv>
 			<BackButton match={match} />
 		</section>
 	);
