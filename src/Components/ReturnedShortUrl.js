651671fb429634b49fb2e7a7314dfda3e239980f
Pere <pere.solaclaver@gmail.com>
2020-10-12 22:37:51 +0100

    Add accordeons to the list of urls returned by the server

--- src/Components/ReturnedShortUrl.js
+++ src/Components/ReturnedShortUrl.js
@@ -1,43 +1,9 @@
 import React, { useRef } from 'react';
-import styled from 'styled-components';
 import { Typography, Button, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 // helpers
 import copyToClipboard from '../Helpers/helpers';
-
-const StyledDiv = styled.div`
-	display: flex;
-	flex-flow: row wrap;
-	justify-content: space-around;
-	align-items: center;
-	width: 80%;
-	margin: 0 0 1rem 0;
-	@media (max-width: 600px) {
-		width: 90%;
-		flex-direction: column;
-	}
-	h4 {
-		color: #187bcd;
-		@media (max-width: 600px) {
-			width: 100%;
-		}
-	}
-	button {
-		flex-grow: 1;
-		border: none;
-		height: 2rem;
-		background-color: white;
-		font-size: 1rem;
-		color: #187bcd;
-		border: 1px solid #187bcd;
-		max-width: 30%;
-		@media (max-width: 600px) {
-			margin: 0.2rem;
-			max-width: 100%;
-		}
-	}
-`;
 
 const useStyles = makeStyles((theme) => ({
 	root: {
